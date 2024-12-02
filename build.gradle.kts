37b6e17484a1ab87917a0175c0b81c3f76f9f86f
MattiaFaroni <mattiafaroni@gmail.com>
2024-12-02 21:51:31 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 import java.util.*
 
+val lombokVersion: String by project
+val postgresVersion: String by project
+val mockitoVersion: String by project
+
 plugins {
     war
     java
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.6"
     id("com.diffplug.spotless") version "6.25.0"
-    id("io.sentry.jvm.gradle") version "4.12.0"
+    id("io.sentry.jvm.gradle") version "4.14.1"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -28,18 +32,18 @@ repositories {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
     implementation("org.geotools:gt-geojson:32.0")
-    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-tomcat")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
-    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
 }
 
 tasks.withType<Test> {
